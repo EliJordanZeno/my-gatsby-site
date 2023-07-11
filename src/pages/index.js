5e0fcbb2f4927f4c270d2a83fa430e754b2b8fe5
EliJordanZeno <eli.jordan@zenogroup.com>
2023-07-11 12:23:43 -0400

    flakjsfk

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
-    <main>
+
       <Layout pageTitle={"Home Page"}>
         <h1>Welcome to my Gatsby site!</h1>
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+          alt="clifford, a reddish-brown pitbull, posing on a couch, looking stoically at the camera"
+          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        />
       </Layout>
-    </main>
+
   )
 }
 
